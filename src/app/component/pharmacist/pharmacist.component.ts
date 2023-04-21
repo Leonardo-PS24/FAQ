8052f69ab10dee3cc1b63534c8ee657b1d4a5025
Leonardo Maran <leonardo.maran@Videosystem.local>
2023-04-21 13:56:53 +0200

    aggiunti commenti

--- src/app/component/pharmacist/pharmacist.component.ts
+++ src/app/component/pharmacist/pharmacist.component.ts
@@ -13,25 +13,35 @@ interface Question {
   styleUrls: ['./pharmacist.component.css']
 
 })
+
 export class PharmacistComponent {
- 
 
+  // A public property results is declared and initialized with an array of data
+  public results = [...data];
   
-public results = [...data];
-mostraDiv(){
-  if (this.results.length < 1) {
-    return true;
-  } else {
-    return false;
+  // A method showDiv is declared which checks if the length of results array is less than 1 and returns true, else returns false
+  showDiv(){
+    if (this.results.length < 1) {
+      return true;
+    } else {
+      return false;
+    }
   }
-}
+  
+  // A method handleInput is declared which takes an event as input
   handleInput(event: Event) {
+  
+    // A variable x is declared and initialized with an HTML element using its ID
     const x = document.getElementById("nothing");
-const query = (event.target as HTMLInputElement)?.value?.toLowerCase();
-this.results = data.filter(d => d.question.toLowerCase().indexOf(query) > -1 || d.answer.toLowerCase().indexOf(query) > -1);
-
     
+    // A variable query is declared and initialized with the lowercased value of the input target
+    const query = (event.target as HTMLInputElement)?.value?.toLowerCase();
+    
+    // The results property is assigned a filtered array based on whether the question or answer property of each object in data contains the query string
+    this.results = data.filter(d => d.question.toLowerCase().indexOf(query) > -1 || d.answer.toLowerCase().indexOf(query) > -1);
   }
 }
+  
+
 
 
